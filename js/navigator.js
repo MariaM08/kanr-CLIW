b472b54665960ced4b736d04d4447647e943d0d0
MariaM08 <marymaryuca@gmail.com>
2018-12-04 01:18:29 +0200

    created navigator js, updated scholarly and fixed some css minor details

---
+++ js/navigator.js
@@ -0,0 +1,22 @@
+function displayElement(element, id){
+    unselectMenu();
+    if ( document.getElementById(id).style.display =='none')
+         {
+             document.getElementById(id).style.display ='block';
+             element.parentNode.classList.add('selected');
+        }
+}
+
+function unselectMenu() {
+    const selectedOthers =  document.getElementsByClassName('selected');
+    for(iterator in selectedOthers) {
+        if(selectedOthers[iterator].classList != undefined)
+            selectedOthers[iterator].classList.remove('selected');                
+    }
+    
+    const displayedOthers =  document.getElementsByClassName('menu-display');
+    for(iterator in displayedOthers) {
+        if(displayedOthers[iterator].style != undefined)
+            displayedOthers[iterator].style.display = 'none';                
+    }
+}
